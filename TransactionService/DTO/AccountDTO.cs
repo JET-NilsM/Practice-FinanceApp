9c0dd7b1058cb35a8b8719015985ed4000c5bfb4
nils-meijer <nils.meijer@justeattakeaway.com>
2025-09-02 09:38:36 +0200

    Expanded service, object mapper, introduced entity. Switched to postgres

--- TransactionService/DTO/AccountDTO.cs
+++ TransactionService/DTO/AccountDTO.cs
@@ -1,9 +1,12 @@
+using TransactionService.Utilities;
+
 namespace TransactionService.DTO;
 
 public class AccountDTO
 {
     public int ID { get; set; }
     public string FullName { get; set; }
-    public string Email { get; set; }
+    [EmailValidation] public string Email { get; set; }
+    public string Password { get; set; }
     public string PhoneNumber { get; set; }
 }
--- TransactionService/Mapper/AccountMapper.cs
+++ TransactionService/Mapper/AccountMapper.cs
@@ -1,17 +1,72 @@
 using TransactionService.DTO;
+using TransactionService.Entities;
 using TransactionService.Models;
 
 namespace TransactionService.Mapper;
 
-public class AccountMapper : IAccountMapper
+public static class AccountMapper
 {
-    public Account MapToModel(AccountDTO dto)
+    public static AccountModel DtoToModel(AccountDTO dto)
     {
-        return new Account
+        if (dto == null)
+        {
+            throw new NullReferenceException("Account DTO is null");
+        }
+        
+        return new AccountModel
         {
             FullName = dto.FullName,
             Email = dto.Email,
+            Password = dto.Password,
             PhoneNumber = dto.PhoneNumber
+        };
+    }
+    
+    public static AccountModel EntityToModel(AccountEntity entity)
+    {
+        if (entity == null)
+        {
+            throw new NullReferenceException("Account entity is null");
+        }
+        
+        return new AccountModel
+        {
+            ID = entity.ID,
+            FullName = entity.FullName,
+            Email = entity.Email,
+            PhoneNumber = entity.PhoneNumber
+        };
+    }
+    
+    public static AccountDTO ModelToDto(AccountModel model)
+    {
+        if (model == null)
+        {
+            throw new NullReferenceException("Account model is null");
+        }
+        
+        return new AccountDTO
+        {
+            ID = model.ID,
+            FullName = model.FullName,
+            Email = model.Email,
+            PhoneNumber = model.PhoneNumber
+        };
+    }
+    
+    public static AccountEntity ModelToEntity(AccountModel model)
+    {
+        if (model == null)
+        {
+            throw new NullReferenceException("Account model is null");
+        }
+        
+        return new AccountEntity
+        {
+            ID = model.ID,
+            FullName = model.FullName,
+            Email = model.Email,
+            PhoneNumber = model.PhoneNumber
         };
     }
 }
